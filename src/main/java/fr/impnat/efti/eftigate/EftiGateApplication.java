2838cb7f4d3bb8bf9c50c2d463e5cdaad16f6723
THUEUX Julien <julien.thueux@ingroupe.com>
2023-11-06 11:33:17 +0100

    add projet

---
+++ src/main/java/fr/impnat/efti/eftigate/EftiGateApplication.java
@@ -0,0 +1,13 @@
+package fr.impnat.efti.eftigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EftiGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EftiGateApplication.class, args);
+    }
+
+}
