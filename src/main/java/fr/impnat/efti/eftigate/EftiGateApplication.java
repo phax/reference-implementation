37687854bcb29e63a89111d7dfa32accef463dc9
Julien Thueux <julien.thueux@ingroupe.com>
2023-11-10 11:33:04 +0100

    Add dependency for swagger + configuration

--- src/main/java/fr/impnat/efti/eftigate/EftiGateApplication.java
+++ src/main/java/fr/impnat/efti/eftigate/EftiGateApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"fr.impnat.efti"}, exclude = {DataSourceAutoConfiguration.class})
