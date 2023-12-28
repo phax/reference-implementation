264f1279c048afe2d65d6356b182cb299c1847ff
dfourmaux <damien.fourmaux@gmail.com>
2023-12-28 11:46:43 +0100

    Init with some enums and dto

---
+++ src/main/java/com/ingroupe/efti/commons/enums/RequestStatusEnum.java
@@ -0,0 +1,13 @@
+package com.ingroupe.efti.commons.enums;
+
+public enum RequestStatusEnum {
+    RECEIVED,
+    SEND_ERROR,
+    IN_PROGRESS,
+    ANSWER_ERROR,
+    ERROR,
+    SUCCESS,
+    TIMEOUT,
+    ARCHIVED,
+    PARTIAL_RESPONSE
+}
