e091df5172fb99d0366db15f13dcd05b82acc956
dfourmaux <damien.fourmaux@gmail.com>
2024-01-03 16:45:54 +0100

    clean swagger + add properties to disable it

--- src/main/java/com/ingroupe/efti/commons/dto/AbstractUilDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/AbstractUilDto.java
@@ -1,6 +1,7 @@
 package com.ingroupe.efti.commons.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -20,18 +21,21 @@ public abstract class AbstractUilDto {
     @NotBlank(message = "UIL_GATE_EMPTY")
     @Size(max = 255, message = "UIL_GATE_TOO_LONG")
     @Pattern(regexp = REGEX_URI, message = "UIL_GATE_INCORRECT_FORMAT")
+    @Schema(example = "regex = \\w+:(\\/?\\/?)[^\\s]+")
     @JsonProperty("eFTIGateUrl")
     private String eFTIGateUrl;
 
     @NotBlank(message = "UIL_UUID_EMPTY")
     @Size(max = 36, message = "UIL_UUID_TOO_LONG")
     @Pattern(regexp = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}", message = "UIL_UUID_INCORRECT_FORMAT")
+    @Schema(example = "regex = [0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")
     @JsonProperty("eFTIDataUuid")
     private String eFTIDataUuid;
 
     @NotBlank(message = "UIL_PLATFORM_EMPTY")
     @Size(max = 255, message = "UIL_PLATFORM_TOO_LONG")
     @Pattern(regexp = REGEX_URI, message = "UIL_PLATFORM_INCORRECT_FORMAT")
+    @Schema(example = "regex = \\w+:(\\/?\\/?)[^\\s]+")
     @JsonProperty("eFTIPlatformUrl")
     private String eFTIPlatformUrl;
 }
