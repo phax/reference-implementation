adb07d3c23639bf8dac3efa0005e29d5095b1e2a
Julien Thueux <julien.thueux@ingroupe.com>
2024-01-22 10:52:10 +0100

    refactor(correction-enum) : correction of enums

--- src/main/java/com/ingroupe/efti/commons/dto/AbstractUilDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/AbstractUilDto.java
@@ -19,24 +19,24 @@ public abstract class AbstractUilDto {
 
     private static final String REGEX_URI = "\\w+:(\\/?\\/?)[^\\s]+";
 
-    @NotNull(message = "UIL_GATE_EMPTY")
-    @NotBlank(message = "UIL_GATE_EMPTY")
+    @NotNull(message = "UIL_GATE_MISSING")
+    @NotBlank(message = "UIL_GATE_MISSING")
     @Size(max = 255, message = "UIL_GATE_TOO_LONG")
     @Pattern(regexp = REGEX_URI, message = "UIL_GATE_INCORRECT_FORMAT")
     @Schema(example = "regex = \\w+:(\\/?\\/?)[^\\s]+")
     @JsonProperty("eFTIGateUrl")
     private String eFTIGateUrl;
 
-    @NotNull(message = "UIL_UUID_EMPTY")
-    @NotBlank(message = "UIL_UUID_EMPTY")
+    @NotNull(message = "UIL_UUID_MISSING")
+    @NotBlank(message = "UIL_UUID_MISSING")
     @Size(max = 36, message = "UIL_UUID_TOO_LONG")
     @Pattern(regexp = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}", message = "UIL_UUID_INCORRECT_FORMAT")
     @Schema(example = "regex = [0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")
     @JsonProperty("eFTIDataUuid")
     private String eFTIDataUuid;
 
-    @NotNull(message = "UIL_PLATFORM_EMPTY")
-    @NotBlank(message = "UIL_PLATFORM_EMPTY")
+    @NotNull(message = "UIL_PLATFORM_MISSING")
+    @NotBlank(message = "UIL_PLATFORM_MISSING")
     @Size(max = 255, message = "UIL_PLATFORM_TOO_LONG")
     @Pattern(regexp = REGEX_URI, message = "UIL_PLATFORM_INCORRECT_FORMAT")
     @Schema(example = "regex = \\w+:(\\/?\\/?)[^\\s]+")
