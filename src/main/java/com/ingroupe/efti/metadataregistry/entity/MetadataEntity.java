8a8bc59dbbc435496becf42260d0a8ef4d3a1090
osouley <ousmane.souley@ext.ingroupe.com>
2024-02-22 18:25:37 +0100

    EFTI-809 : PR review corrections

--- src/main/java/com/ingroupe/efti/metadataregistry/entity/MetadataEntity.java
+++ src/main/java/com/ingroupe/efti/metadataregistry/entity/MetadataEntity.java
@@ -1,10 +1,32 @@
 package com.ingroupe.efti.metadataregistry.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.ingroupe.efti.commons.enums.CountryIndicator;
 import com.ingroupe.efti.commons.model.AbstractModel;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,9 +49,17 @@ public class MetadataEntity extends AbstractModel {
     private String eFTIDataUuid;
     private String eFTIGateUrl;
     private boolean isDangerousGoods;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'+'SSSS")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'+'SSSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime journeyStart;
     @Enumerated(EnumType.STRING)
     private CountryIndicator countryStart;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'+'SSSS")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'+'SSSS")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime journeyEnd;
     @Enumerated(EnumType.STRING)
     private CountryIndicator countryEnd;
