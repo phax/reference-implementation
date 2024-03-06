23477ea3b86e5640c2b52063122cf3b9e444182e
osouley <ousmane.souley@ext.ingroupe.com>
2024-03-06 16:49:52 +0100

    EFTI-908 : made abstract model serializable

--- src/main/java/com/ingroupe/efti/commons/model/AbstractModel.java
+++ src/main/java/com/ingroupe/efti/commons/model/AbstractModel.java
@@ -6,11 +6,12 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Data
-public abstract class AbstractModel {
+public abstract class AbstractModel implements Serializable {
 
     /**
      * date that the data have been created
