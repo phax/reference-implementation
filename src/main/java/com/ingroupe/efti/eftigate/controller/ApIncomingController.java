28151050e4cb0d417d56d73754d4d67d365962c3
dfourmaux <damien.fourmaux@gmail.com>
2024-04-05 10:21:54 +0200

    send response to domibus call

--- src/main/java/com/ingroupe/efti/eftigate/controller/ApIncomingController.java
+++ src/main/java/com/ingroupe/efti/eftigate/controller/ApIncomingController.java
@@ -2,13 +2,10 @@ package com.ingroupe.efti.eftigate.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ingroupe.efti.edeliveryapconnector.dto.ReceivedNotificationDto;
-import com.ingroupe.efti.eftigate.service.ApIncomingService;
 import com.ingroupe.efti.eftigate.service.RabbitSenderService;
 import io.swagger.v3.oas.annotations.Hidden;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ApIncomingController {
 
+    private final String soapResult = """
+            <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+               <Body> domibus ws plugin require a response when it call our endpoint </Body>
+            </Envelope>
+           """;
+
     private final RabbitSenderService rabbitSenderService;
 
     @Value("${spring.rabbitmq.queues.eftiReceiveMessageExchange:efti.send-message.exchange}")
@@ -32,13 +35,13 @@ public class ApIncomingController {
     private String eftiKeySendMessage;
 
     @PostMapping("/notification")
-    public ResponseEntity<Void> incoming(final @RequestBody ReceivedNotificationDto receivedNotificationDto) {
+    public ResponseEntity<String> incoming(final @RequestBody ReceivedNotificationDto receivedNotificationDto) {
         log.info("receive notification from domibus");
         try {
             rabbitSenderService.sendMessageToRabbit(eftiReceiveMessageExchange, eftiKeySendMessage, receivedNotificationDto);
         } catch (JsonProcessingException e) {
             log.error("Error when try to parse message and send it to the rabbitmq", e);
         }
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(soapResult);
     }
 }
