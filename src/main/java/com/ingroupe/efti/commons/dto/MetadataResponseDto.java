28edeb110c6e7a3541a3bf3f1972e66744fa9759
dfourmaux <damien.fourmaux@gmail.com>
2024-05-13 11:04:01 +0200

    fix dto for xml

--- src/main/java/com/ingroupe/efti/commons/dto/MetadataResponseDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/MetadataResponseDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.ingroupe.efti.commons.enums.CountryIndicator;
 import com.ingroupe.efti.commons.enums.StatusEnum;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +24,7 @@ import java.util.List;
         "metadata"
 })
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@XmlRootElement(name = "body")
 public class MetadataResponseDto {
     private CountryIndicator eFTIGate;
     private String requestUuid;
