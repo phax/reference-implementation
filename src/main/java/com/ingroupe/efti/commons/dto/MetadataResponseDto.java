1a12d22050b2a0b2d7a060ae681142211e97a687
dfourmaux <damien.fourmaux@gmail.com>
2024-05-23 16:19:05 +0200

    feat() fix field name + dont return it for REST response

--- src/main/java/com/ingroupe/efti/commons/dto/MetadataResponseDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/MetadataResponseDto.java
@@ -1,9 +1,11 @@
 package com.ingroupe.efti.commons.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.ingroupe.efti.commons.enums.CountryIndicator;
 import com.ingroupe.efti.commons.enums.StatusEnum;
+import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +28,8 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @XmlRootElement(name = "body")
 public class MetadataResponseDto {
+    @JsonProperty("eFTIGate")
+    @XmlElement(name = "eFTIGate")
     private CountryIndicator eFTIGate;
     private String requestUuid;
     private StatusEnum status;
