ef5cd6cd4815c3002bee2cdb3674598280c0d9f5
osouley <ousmane.souley@ext.ingroupe.com>
2024-05-30 17:43:03 +0200

    ignore disabled attribute

--- src/main/java/com/ingroupe/efti/commons/dto/MetadataResultDto.java
+++ src/main/java/com/ingroupe/efti/commons/dto/MetadataResultDto.java
@@ -37,5 +37,6 @@ public class MetadataResultDto implements Serializable {
     @NotEmpty(message = "TRANSPORT_VEHICLES_MISSING")
     @Valid
     private List<TransportVehicleDto> transportVehicles;
+    @JsonIgnore
     private boolean isDisabled;
 }
