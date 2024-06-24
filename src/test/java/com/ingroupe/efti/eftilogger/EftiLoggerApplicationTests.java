a738a19c03647cd10c1163e3dd23c74d3dd63fee
THUEUX Julien <julien.thueux@ingroupe.com>
2024-06-24 14:53:21 +0200

    firstu push

---
+++ src/test/java/com/ingroupe/efti/eftilogger/EftiLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingroupe.efti.eftilogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EftiLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
